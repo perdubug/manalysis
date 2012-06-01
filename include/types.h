77d59366d7b8e3c681546d12e9e42058319ae2f3
Ming Yang <ming.3.yang@nokia.com>
2012-06-01 16:57:54 +0800

    Missed to add typeds.h for thread pool patch,sorry

---
+++ include/types.h
@@ -0,0 +1,14 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/************************************************************************** 
+   Data types...
+ **************************************************************************/
+typedef unsigned char      uint8;
+typedef short              uint16;
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+typedef signed int         sint32;
+
+#endif
+
